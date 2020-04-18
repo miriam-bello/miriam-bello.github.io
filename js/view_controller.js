35ea08383f8ecbb96712126857428efa9ca1620c
Miriam <miriambellodavila@gmail.com>
2020-04-18 16:11:21 +0200

    Botones bonitos, imagenes jpg y sección de cv

--- js/view_controller.js
+++ js/view_controller.js
@@ -1,3 +1,19 @@
+//Eventos de click para mostrar secciones de galería, contacto y currículum
 document.getElementById('contact-info').style.display = "none";
-document.getElementById('contact-button').addEventListener('click', () => { document.getElementById('gallery').style.display = "none"; document.getElementById('contact-info').style.display = null; });
-document.getElementById('gallery-button').addEventListener('click', () => { document.getElementById('gallery').style.display = null; document.getElementById('contact-info').style.display = "none"; });
+document.getElementById('cv-info').style.display = "none";
+
+document.getElementById('contact-button').addEventListener('click', () => {
+    document.getElementById('gallery').style.display = "none";
+    document.getElementById('cv-info').style.display = "none";
+    document.getElementById('contact-info').style.display = null;
+});
+document.getElementById('gallery-button').addEventListener('click', () => {
+    document.getElementById('contact-info').style.display = "none";
+    document.getElementById('cv-info').style.display = "none";
+    document.getElementById('gallery').style.display = null; 
+});
+document.getElementById('cv-button').addEventListener('click', () => {
+    document.getElementById('gallery').style.display = "none";
+    document.getElementById('contact-info').style.display = "none"; 
+    document.getElementById('cv-info').style.display = null;
+});
